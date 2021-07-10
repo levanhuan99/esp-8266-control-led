b82aa531d9b2faf963d080bbad96ad5421c39a00
vanhuan <vanhuan28101999@gmail.com>
2021-07-10 16:21:21 +0700

    turn on/off led

---
+++ esp8266.ino
@@ -0,0 +1,99 @@
+/*
+ *  Simple hello world Json REST response
+  *  by Mischianti Renzo <https://www.mischianti.org>
+ *
+ *  https://www.mischianti.org/
+ *
+ */
+ 
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+ 
+const char* ssid = "Vanhuan";
+const char* password = "duaanh3lit";
+int led = 2;
+
+ESP8266WebServer server(80);
+ 
+// Serving Hello world
+void getHelloWord() {
+
+    server.send(200, "text/json", "{\"status\": \"200\"}");
+}
+ 
+// Define routing
+void restServerRouting() {
+    server.on("/", HTTP_GET, []() {
+        server.send(200, F("text/html"),
+            F("Welcome to the REST Web Server"));
+    });
+    server.on(F("/turn-on-light"), HTTP_GET, getHelloWord);
+    
+}
+ 
+// Manage not found URL
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+ 
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");  
+  pinMode(led,OUTPUT);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  // Activate mDNS this is used to be able to connect to the server
+  // with local DNS hostmane esp8266.local
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+ 
+  // Set server routing
+  restServerRouting();
+  // Set not found response
+  server.onNotFound(handleNotFound);
+  //led on
+  server.on("/led-on",setLedOn);
+    //led off
+  server.on("/led-off",setLedOnff);
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void setLedOn(){
+    digitalWrite(led,HIGH);
+    server.send(204,"led is on now");
+}
+void setLedOnff(){
+    digitalWrite(led,LOW);
+    server.send(204,"led is off now");
+}
+void loop(void) {
+  server.handleClient();
+}
